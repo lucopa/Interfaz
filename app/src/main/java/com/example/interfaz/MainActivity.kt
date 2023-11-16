bf852641a53e803e29e2c6d04d02c3358a2879ee
lucopa <lcobpar754@g.educaand.es>
2023-11-16 22:01:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/interfaz/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.interfaz
+
+
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock
+import android.widget.Button
+import android.widget.ImageButton
+
+import java.util.Calendar
+
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation", "MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val boton : ImageButton = findViewById(R.id.llamada)
+        boton.setOnClickListener{
+            val intent: Intent = Intent (this, Pantalla2:: class.java)
+            startActivity(intent)
+        }
+
+
+        val alarma: ImageButton = findViewById(R.id.botonAlarma)
+        alarma.setOnClickListener() {
+            abrirAlarma()
+
+        }
+
+        val botonURL: ImageButton = findViewById(R.id.url)
+        botonURL.setOnClickListener {
+            abrirURL("https://www.wikipedia.com")
+        }
+
+        val btnOpenYouTube: ImageButton = findViewById(R.id.otro)
+        btnOpenYouTube.setOnClickListener {
+            abrirYoutube()
+        }
+    }
+
+
+    private fun abrirYoutube() {
+        val youtubeUri = Uri.parse("https://www.youtube.com")
+        val youtubeIntent = Intent(Intent.ACTION_VIEW, youtubeUri)
+
+        youtubeIntent.setPackage("com.google.android.youtube")
+
+        if (youtubeIntent.resolveActivity(packageManager) != null) {
+            startActivity(youtubeIntent)
+        } else {
+
+            val webIntent = Intent(Intent.ACTION_VIEW, youtubeUri)
+            startActivity(webIntent)
+        }
+    }
+    private fun abrirURL(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
+
+    private fun abrirAlarma() {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, "Alarma")
+            putExtra(AlarmClock.EXTRA_HOUR, Calendar.getInstance().get(Calendar.HOUR_OF_DAY))
+            putExtra(AlarmClock.EXTRA_MINUTES, Calendar.getInstance().get(Calendar.MINUTE) + 2)
+        }
+        startActivity(intent)
+    }
+
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/interfaz/Pantalla2.kt
@@ -0,0 +1,51 @@
+package com.example.interfaz
+
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+class Pantalla2 : AppCompatActivity() {
+    private val REQUEST_CALL_PERMISSION = 1
+
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla2)
+
+        val botonLlamada: ImageButton = findViewById(R.id.boton_llamada)
+        botonLlamada.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CALL_PHONE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                hacerLlamada()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    REQUEST_CALL_PERMISSION
+                )
+            }
+        }
+    }
+
+
+    private fun hacerLlamada() {
+        val numeroTelefono = "tel:674695275"
+        val intentLlamada = Intent(Intent.ACTION_CALL, Uri.parse(numeroTelefono))
+        startActivity(intentLlamada)
+    }
+
+
+
+
+
+}
